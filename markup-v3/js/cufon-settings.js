f4dd34371541c73c088cc8d55b0f4bdce7d7a346
Ruslan Grigoryev <rus.grigoryev@yandex.ua>
2014-05-18 12:02:23 +0300

    Добавил страницу новостей

--- markup-v3/js/cufon-settings.js
+++ markup-v3/js/cufon-settings.js
@@ -1,5 +1,5 @@
 function initCufon() {
-	Cufon.replace('h1, .content-block h2, .content-block h3, #news-block h2, #news-block h3, .history h2, .ttl-trafic, .ttl-promo, .ttl-payments, .ttl-privacy, .ttl-balance', { fontFamily: 'AleksandraC', hover: true });
+	Cufon.replace('h1, .content-block h2, .content-block h3, #news-block h2, #news-block h3, .history h2, .ttl-trafic, .ttl-promo, .ttl-payments, .ttl-privacy, .ttl-balance, .ttl-news', { fontFamily: 'AleksandraC', hover: true });
 
 }
 
